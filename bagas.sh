cfcbe17bf44efbaa82a6a26493d6b35850d46b52
Dwi Bagas Prayoga <89993139+BagaszPraa@users.noreply.github.com>
2024-08-20 12:31:43 +0700

    Create bagas.sh

---
+++ bagas.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/home/bagas/MAVkit/build/mavkit --tty 57600 /dev/FCU --udp_client 192.168.144.100 19222
+# /home/ati/MAVkit/build/mavkit --tty 57600 /dev/FCU --udp_client 192.168.168.82 14550
+
